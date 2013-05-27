8452615a4f09872c9913ccae8913d4acc52e0e85
vmlaker <velimir.mlaker@gmail.com>
2013-05-27 11:41:58 -0700

    Add read-file scenario.

--- test/test1.py
+++ test/test1.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from coils import Config
-c = Config()
-c['NAME'] = 'Bender'
-c['SURNAME'] = 'Rodríguez'
-print(c)
+c1 = Config()
+c1['NAME'] = 'Bender'
+c1['SURNAME'] = 'Rodríguez'
+print(c1)
+
+from os.path import join, dirname, abspath
+fname = join(dirname(abspath(__file__)), 'test1.cfg')
+c2 = Config(fname)
+c2['FRUIT'] = 'apple'
+print(c2['FRUIT'])
+print(c2)
