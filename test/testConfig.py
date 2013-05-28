421aa4e54fa8f6d2acb6fe62cf3528624fb22ed2
vmlaker <velimir.mlaker@gmail.com>
2013-05-28 11:03:14 -0700

    Rename test files.

--- test/testConfig.py
+++ test/testConfig.py
@@ -13,7 +13,7 @@ def test2():
     """Read from file and get."""
     from os.path import join, dirname, abspath
     from coils import Config
-    fname = join(dirname(abspath(__file__)), 'test1.cfg')
+    fname = join(dirname(abspath(__file__)), 'simple.cfg')
     c = Config(fname)
     assert c['TOON'] == 'Bugs Bunny'
     assert c['QUOTE'] == 'The quick brown fox jumped over the lazy dog.'
