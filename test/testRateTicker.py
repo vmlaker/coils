3ad9eb487c208338996c9b48c04034d4a30f808d
vmlaker <velimir.mlaker@gmail.com>
2013-05-28 11:43:33 -0700

    Remove unused time import.

--- test/testRateTicker.py
+++ test/testRateTicker.py
@@ -1,13 +1,11 @@
 def test1():
     """Tick a simple ticker."""
-    import time
     from coils import RateTicker
     a = RateTicker((1,))
     assert a.tick() == (1.0,)
 
 def test2():
     """Tick a three-period ticker."""
-    import time
     from coils import RateTicker
     a = RateTicker((1,2,5,))
     assert a.tick() == (1.0, 0.5, 0.2)
