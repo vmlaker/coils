8dffc4803e41aeb49ee21f5d6c15909e70465a45
vmlaker <velimir.mlaker@gmail.com>
2013-05-31 16:19:44 -0700

    Add hack for nose.

---
+++ test/test000.py
@@ -0,0 +1,19 @@
+def test():
+    """
+    Hack to get nose to run tests properly.
+    This file should be the first file processed by a nosetests run.
+    """
+
+    # Add parent directory to system path
+    # as per answer http://stackoverflow.com/a/1054293
+    # to question http://stackoverflow.com/q/1054271 .
+    import sys
+    import os
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__), 
+                os.path.pardir,
+                )
+            )
+        )
