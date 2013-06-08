440b311e22df22be05d1b61336a9a9e43941b3f2
vmlaker <velimir.mlaker@gmail.com>
2013-06-08 09:57:55 -0700

    Add a Config test with embedded (recoverable) error.

--- test/testConfig.py
+++ test/testConfig.py
@@ -17,3 +17,12 @@ def test2():
     c = Config(fname)
     assert c['TOON'] == 'Bugs Bunny'
     assert c['QUOTE'] == 'The quick brown fox jumped over the lazy dog.'
+
+def test3():
+    """Read from file containing erroneous line, and get."""
+    from os.path import join, dirname, abspath
+    from Config import Config
+    fname = join(dirname(abspath(__file__)), 'simple2.cfg')
+    c = Config(fname)
+    assert c['Kingdom'] == 'Animalia'
+    assert c['Genus'] == 'Python'
