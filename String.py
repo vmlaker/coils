92c91072ca0afec37151fdc5b1f56bca0c09eeb9
vmlaker <velimir.mlaker@gmail.com>
2013-06-15 16:58:57 -0700

    Add conversion from time to levels, dir and fname.

--- String.py
+++ String.py
@@ -1,5 +1,6 @@
 """Useful string conversions and such."""
 
+from os.path import join
 from datetime import datetime
 
 MICRO = '.%f'
@@ -18,3 +19,24 @@ def time2string(tstamp, micro=True):
     """Return time string, given a datetime object."""
     tformat = TIME_FORMAT if micro else TIME_FORMAT[:-len(MICRO)]
     return tstamp.strftime(tformat)
+
+def time2levels(tstamp):
+    """Return a list of directory levels (as strings) given a datetime object.
+         E.g. given 2011-09-25 13:01:44,
+              return ['2011', '09', '25', '13', '01'] 
+    """
+    return [tstamp.strftime(xx) for xx in ('%Y', '%m', '%d', '%H', '%M')]
+
+def time2dir(tstamp):
+    """Return assembly of os.path.join()s for given tstamp."""
+    result = ''
+    for field in time2levels(tstamp):
+        result = join(result, field)
+    return result
+
+FILENAME_FORMAT = '__%Y-%m-%d__%H:%M:%S:%f__'
+
+def time2fname(tstamp, full=False):
+    result = tstamp.strftime(FILENAME_FORMAT)
+    result = result if not full else join(time2dir(tstamp), result)
+    return result
--- test/testString.py
+++ test/testString.py
@@ -27,3 +27,33 @@ def test2():
     a = datetime.now()
     assert '{0}'.format(a) == '{0}'.format(time2string(a))
 test2()
+
+def test3():
+    """Test time2levels() function."""
+    from datetime import datetime
+    from String import time2levels, string2time
+    t1 = datetime.now()
+    assert len(time2levels(t1)) == 5
+    t2 = string2time('2013-06-15 16:30:18')
+    assert time2levels(t2) == ['2013', '06', '15', '16', '30']
+test3()
+
+def test4():
+    """Test time2dir() function."""
+    from os.path import join
+    from datetime import datetime
+    from String import time2dir, string2time
+    t1 = string2time('2013-06-15 16:30:18')
+    assert time2dir(t1) == join('2013', '06', '15', '16', '30')
+test4()
+
+def test5():
+    """Test time2fname() function."""
+    from os.path import join
+    from datetime import datetime
+    from String import time2fname, string2time
+    t1 = string2time('2013-06-15 16:30:18')
+    assert time2fname(t1) == '__2013-06-15__16:30:18:000000__'
+    assert time2fname(t1, full=True) == '\
+2013/06/15/16/30/__2013-06-15__16:30:18:000000__'
+test5()
