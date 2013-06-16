3ee7d716f0eb3202ccf7ca213747eb903f9bb471
vmlaker <velimir.mlaker@gmail.com>
2013-06-15 17:09:52 -0700

    Add missing names to module namespace.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from .Config import Config
 from .RateTicker import RateTicker
 from .Ring import Ring
 from .SortedList import SortedList
-from .String import string2time, time2string
+from .String import string2time, time2string, time2levels, time2dir, time2fname
 from .Timer import Timer
 from .UserInput import user_input
