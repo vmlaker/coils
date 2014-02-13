6765db3a5cecf985c4a81f763a808490b4e4d311
vmlaker <velimir.mlaker@gmail.com>
2014-02-13 09:16:08 -0800

    Update gh-pages build.

--- doc/build-gh-pages.sh
+++ doc/build-gh-pages.sh
@@ -5,6 +5,8 @@
 git checkout gh-pages
 
 # Remove everything execept the logo.
+# TODO: Change gh-pages to use logo in master branch (raw)
+#       so we don't have to pick-n-choose here.
 rm -rf *.html *.js objects.inv _static _sources
 
 # Switch to master branch (Sphinx build needs doc/.)
@@ -19,10 +21,10 @@ cd ..
 mv -fv doc/build/html/* .
 
 # Remove the directories (from master branch) needed for building docs.
-rm -rf doc
+rm -rf setup.py src build dist doc
 
 # Add everything to gh-pages.
-git add .
+git add --all
 
 # Commit with comment referencing latest master branch commit.
 git commit -m "Updated gh-pages for `git log master -1 | head -1`"
