504731e0cf49e144d5febaf2fc196f3007b3bd79
vmlaker <velimir.mlaker@gmail.com>
2014-02-13 09:06:27 -0800

    Fix documentation versioning, change gh-pages build.

--- doc/build-gh-pages.sh
+++ doc/build-gh-pages.sh
@@ -3,21 +3,23 @@
 
 # Switch to gh-pages branch and start afresh.
 git checkout gh-pages
-#rm -rf *
+
+# Remove everything execept the logo.
+rm -rf *.html *.js objects.inv _static _sources
 
 # Switch to master branch (Sphinx build needs doc/.)
-git checkout master doc
+git checkout master setup.py src doc
 git reset HEAD
 
-# Clone the repo.
-git clone http://github.com/vmlaker/coils
-
 # Build the docs and move html/ files to root directory.
-make -C doc html
+python setup.py bdist
+cd doc
+sh build-doc.sh
+cd ..
 mv -fv doc/build/html/* .
 
 # Remove the directories (from master branch) needed for building docs.
-rm -rf doc coils
+rm -rf doc
 
 # Add everything to gh-pages.
 git add .
